b0882b7248499bcbaa9da54fb0e14c106aab24bf
Konovalov Alexander <konovalov_a.s@mail.ru>
2020-09-16 06:24:59 +0500

    Добавляет валидацию формы

--- source/js/main.js
+++ source/js/main.js
@@ -4,10 +4,61 @@
   var nojs = document.querySelector('.nojs');
   nojs.classList.remove('nojs');
 
+  // toggle main menu
   var mainNav = document.querySelector('.main-nav');
   var menuToggle = document.querySelector('.main-menu-toggle');
   menuToggle.addEventListener('click', function () {
     mainNav.classList.toggle('main-nav--closed');
+  });
+
+  // validate form
+  var helpForm = document.querySelector('.help-form form');
+  var nameInput = helpForm.querySelector('#name');
+  var phoneInput = helpForm.querySelector('#phone');
+
+  var validateName = function () {
+    if (nameInput.validity.valueMissing) {
+      nameInput.setCustomValidity('Как тебя зовут?');
+      return;
+    }
+    if (nameInput.validity.tooShort) {
+      nameInput.setCustomValidity('Не меньше ' + nameInput.minLength + ' символов');
+      return;
+    }
+    nameInput.setCustomValidity('');
+  };
+
+  var validatePhone = function () {
+    if (phoneInput.validity.valueMissing) {
+      phoneInput.setCustomValidity('Укажи номер телефона');
+      return;
+    }
+    if (phoneInput.validity.patternMismatch) {
+      phoneInput.setCustomValidity('Укажи 10 цифр номера телефона без 8 или +7');
+      return;
+    }
+    phoneInput.setCustomValidity('');
+  };
+
+  nameInput.addEventListener('invalid', function () {
+    validateName();
+  });
+
+  nameInput.addEventListener('input', function () {
+    validateName();
+  });
+
+  phoneInput.addEventListener('invalid', function () {
+    validatePhone();
+  });
+
+  phoneInput.addEventListener('input', function () {
+    validatePhone();
+  });
+
+  helpForm.addEventListener('submit', function () {
+    validateName();
+    validatePhone();
   });
 
 })();
